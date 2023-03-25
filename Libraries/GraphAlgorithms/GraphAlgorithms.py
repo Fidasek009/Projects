81e7bc594c9063e6a14c68d2cc4c8491e62a9add
Fidasek009 <102549834+Fidasek009@users.noreply.github.com>
2023-03-25 22:43:16 +0100

    finally works :P

--- Libraries/GraphAlgorithms/GraphAlgorithms.py
+++ Libraries/GraphAlgorithms/GraphAlgorithms.py
@@ -1,11 +1,13 @@
-from typing import List
+from typing import List, Union
+
+INF = float('inf')
 
 # ===================================== EDGE CLASS =====================================
 class Edge:
     vertex: int
     weight: int
 
-    # weighted graph
+
     def __init__(self, v: int, w: int):
         self.vertex = v
         self.weight = w
@@ -13,46 +15,76 @@ class Edge:
 # ===================================== GRAPH CLASS =====================================
 
 class Graph():
-    vertex_count: int           # number of verticies
+    vertex_count: int           # number of vertices
     isDirected: bool            # wherther a graph is directed or not
-    verticies: List[List[Edge]] # the structure of the graph
+    vertices: List[List[Edge]]  # the structure of the graph
 
 
+    # --------------------------------- CLASS MANIPULATION ---------------------------------
     # constructor
-    def __init__(self, vertex_count: int, isDirected: bool = False) -> None:
-        self.vertex_count = vertex_count
+    def __init__(self, vertexes: Union[int, dict[List[List[int]]], dict[List[int]]], isDirected: bool = False) -> None:
         self.isDirected = isDirected
-        self.verticies = [[] for i in range(vertex_count)]
-        #self.visited = []
+        if type(vertexes) == int:
+            self.vertex_count = vertexes
+            self.vertices = [[] for i in range(vertexes)]
+        else:
+            self.vertex_count = len(vertexes)
+            self.build(vertexes)
 
 
-    # overrides string to print a class
+    # overrides string to print the class
     def __str__(self) -> str:
         s = "VERTEX -> (VERTEX|WEIGHT)\n"
         for i in range(self.vertex_count):
             s += str(i) + " -> "
-            for edge in self.verticies[i]:
+            for edge in self.vertices[i]:
                 s += "(" + str(edge.vertex) + "|" + str(edge.weight) + "), "
             s += "\n"
         
         return s
 
+
     # --------------------------------- ADD/REMOVE EDGE ---------------------------------
     def add_edge(self, a: int, b: int, weight: int = 1) -> None:
-        self.verticies[a].append(Edge(b, weight))
+        self.vertices[a].append(Edge(b, weight))
         if not self.isDirected: # reverse the direction
-            self.verticies[b].append(Edge(a, weight))
+            self.vertices[b].append(Edge(a, weight))
 
 
     def delete_edge(self, a: int, b: int) -> None:
-        for edge in self.verticies[a]:
+        for edge in self.vertices[a]:
             if edge.vertex == b: 
-                self.verticies[a].remove(edge)
+                self.vertices[a].remove(edge)
         
-        if not self.isDirected: # delete opposite direction as well
-            for edge in self.verticies[b]:
+        if not self.isDirected:
+            for edge in self.vertices[b]:
                 if edge.vertex == a: 
-                    self.verticies[b].remove(edge)
+                    self.vertices[b].remove(edge)
+
+
+    # --------------------------------- BUILD GRAPH ---------------------------------
+    def build(self, graph: Union[dict[List[List[int]]], dict[List[int]]]) -> None:
+        self.vertices = [[] for i in range(self.vertex_count)]
+        for vertex in graph:
+            for edge in graph[vertex]:
+                try:
+                    self.add_edge(vertex, edge[0], edge[1]) # weighted edge
+                except:
+                    self.add_edge(vertex, edge) # unweighted edge   
+
+
+    # --------------------------------- FIND PATH IN TREE ---------------------------------
+    def Path(self, start: int, finish: int, tree: List[int]) -> List[int]:
+        path: List[int] = []
+
+        if tree[finish] != -1:  # start != finish
+            while finish != start:
+                path.append(finish)
+                finish = tree[finish]
+            path.append(start)
+            path.reverse()
+
+        return path
 
 
     # --------------------------------- BREADTH FIRST SEARCH ---------------------------------
@@ -62,67 +94,76 @@ class Graph():
         queue: List[int] = []
         v: int # current vertex
 
-        visited[start] = True # set the starting vertex as visited
-        queue.append(start)
+        visited[start] = True
+        queue.append(start) # put the start in the queue
 
         while queue:
             v = queue.pop(0)
 
-            for edge in self.verticies[v]:
-                vertex = edge.vertex
-                if not visited[vertex]:
-                    visited[vertex] = True
-                    tree[vertex] = v
-                    queue.append(vertex)
-        
-        """
-        # print the tree
-        for i in range(1, self.vertex_count+1):
-            if tree[self.vertex_count-i] != -1: # except unreachable vertexes
-                print(str(tree[self.vertex_count-i]) + "->" + str(self.vertex_count-i))
-        """
+            for edge in self.vertices[v]:
+                if not visited[edge.vertex]:
+                    visited[edge.vertex] = True
+                    tree[edge.vertex] = v
+                    queue.append(edge.vertex)
 
         return tree
     
 
     def BFS(self, start: int, finish: int) -> List[int]:
-        tree: List[int] = self.BFS_tree(start)
-        path: List[int] = []
+        tree: List[int] = self.BFS_tree(start) # get the tree from BFS algorithm
+        return self.Path(start, finish, tree)
 
-        if tree[finish] != -1:
-            while finish != start:
-                path.append(finish)
-                finish = tree[finish]
-            path.append(start)
-            path.reverse()
 
-        return path
-    
     # --------------------------------- DEPTH FIRST SEARCH ---------------------------------
-    # TODO
+    def DFS_tree(self, start: int) -> List[int]:
+        visited: List[bool] = [False for i in range(self.vertex_count)]
+        tree: List[int] = [-1 for i in range(self.vertex_count)]
+        stack: List[int] = []
+        v: int # current vertex
+
+        stack.append(start) # put the start in the stack
+
+        while stack:
+            v = stack.pop() # take the top item on stack
+
+            if not visited[v]:
+                visited[v] = True
+
+                for edge in self.vertices[v]:
+                    stack.append(edge.vertex)
+                    if not visited[edge.vertex]: tree[edge.vertex] = v
+            
+        return tree
 
 
+    def DFS(self, start: int, finish: int) -> List[int]:
+        tree: List[int] = self.DFS_tree(start) # get the tree from DFS algorithm
+        return self.Path(start, finish, tree)
 
 
+    # --------------------------------- DIJKSTRA SEARCH ---------------------------------
+    def Dijkstra_tree(self, start: int) -> List[int]:
+        distance: List[int] = [INF for i in range(self.vertex_count)] # distance from start node
+        tree: List[int] = [-1 for i in range(self.vertex_count)]
+        queue: List[int] = []
+        v: int # current vertex
+
+        distance[start] = 0
+        queue.append(start)
+
+        while queue:
+            v = queue.pop(0)
+
+            for edge in self.vertices[v]:
+                if distance[v] + edge.weight < distance[edge.vertex]:
+                    distance[edge.vertex] = distance[v] + edge.weight
+                    tree[edge.vertex] = v
+                    queue.append(edge.vertex)
+        
+        print(distance)
+        return tree
 
 
-
-
-# TODO: make graph from Dict[Set()]
-
-if __name__ == "__main__":
-    
-    g =  Graph(8)
-
-    g.add_edge(0, 1)
-    g.add_edge(1, 2)
-    g.add_edge(0, 3)
-    g.add_edge(1, 3)
-    g.add_edge(2, 4)
-    g.add_edge(4, 5)
-    g.add_edge(4, 6)
-    g.add_edge(5, 7)
-    g.add_edge(1, 6)
-
-    print(g.BFS(5, 0))
-    print(g)
+    def Dijkstra(self, start: int, finish: int) -> List[int]:
+        tree: List[int] = self.Dijkstra_tree(start) # get the tree from Dijkstra algorithm
+        return self.Path(start, finish, tree)
